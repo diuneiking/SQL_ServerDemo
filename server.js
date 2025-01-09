d9d32caef353afa7c4f12444f1e536b68390a02f
King <kinglee@hiddenlangkawi.com>
2025-01-09 16:10:24 +0800

    Remove Raw Data

--- server.js
+++ server.js
@@ -14,28 +14,16 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// // Database connection
-// const db = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   waitForConnections: true,
-//   connectionLimit: 50,
-//   queueLimit: 0
-// });
-
-// // Database connection
+// Database connection
 const db = mysql.createPool({
-  host: 'srv1627.hstgr.io',
-  user: 'u461355420_superadmin',
-  password: 'Heehee@2024',
-  database: 'u461355420_heehee',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10,
+  connectionLimit: 50,
   queueLimit: 0
 });
-
 
 // Logging middleware for debugging
 app.use((req, res, next) => {
